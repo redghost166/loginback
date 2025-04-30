5f8c5e8de4e8c24a349cda3ae0b76f234f8afb7f
GUSTAVO GERALDES PEREIRA FILHO <gustavo_pereira-f@estudante.sesisenai.org.br>
2025-04-30 09:06:31 -0300

    first commit

---
+++ src/main/java/com/example/login/infra/security/TokenService.java
@@ -0,0 +1,25 @@
+package com.example.login.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.login.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return "access_token=" + JWT.create();//apagar
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("error while authentication");
+        }
+    }
+}
---
+++ src/main/java/com/example/login/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.login.repositories;
+
+import com.example.login.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
